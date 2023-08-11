a5d8aaaf60201c2e9edd75bc40153af9c778fa23
jjsh03 <aberrant03@gmail.com>
2023-08-11 14:49:08 +0900

    study: ejs 활용'

--- 08-EXPRESS/app.js
+++ 08-EXPRESS/app.js
@@ -5,6 +5,10 @@ const PORT = 8080;
 app.set('view engine', 'ejs'); // express 템플릿 엔진 종류 등록
 app.set('views', './views'); // 템플릿 엔진 파일을 저장할 위치 등록
 
+// (임시) 데이터베이스에서 가져온 회원 정보(id, pw)
+const idFromDB = 'banana';
+const pwFromDB = '1234qwer';
+
 // app.get(경로, 해당 경로로 들어왔을 때 실행할 함수)
 // '/': 서버주소:포트번호/ (localhost:8080/)
 app.get('/', function (request, response) {
@@ -12,7 +16,16 @@ app.get('/', function (request, response) {
   //   response.send('<h1>Hello! Express!</h1>');
 
   // response.render(ejs_filename):eje file이름을 찾아서 응답
-  response.render('index');
+  response.render('index', {
+    userID: idFromDB,
+    userPW: pwFromDB,
+    btns: ['버튼1', '버튼2', '버튼3'],
+    me: {
+      name: 'sage',
+      msg: '저는 세이지입니다.',
+    },
+    isLogin: false,
+  });
 });
 
 // '/sesac' 경로로 들어왔을 때 "새싹 영등포캠퍼스 5기 수업중" 메세지 보이기
