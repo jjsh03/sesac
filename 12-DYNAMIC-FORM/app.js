95d00f968fc051580f36637c49d4a7640dfffe3a
jjsh03 <aberrant03@gmail.com>
2023-08-18 17:16:48 +0900

    study: 동적 폼 전송 - axios, fetch

--- 12-DYNAMIC-FORM/app.js
+++ 12-DYNAMIC-FORM/app.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const app = express();
-const PORT = 8080;
+const PORT = 8000;
 
 app.set('view engine', 'ejs');
-app.set('/views', 'views');
-
+app.use('/views', express.static(__dirname + '/views'));
 app.use(express.urlencoded({ extended: true }));
-app.unsubscribe(express.json());
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.render('dynamic');
@@ -22,6 +21,28 @@ app.post('/ajax', (req, res) => {
   res.send(req.body);
 });
 
-app.listen(PORT, () => {
-  console.log(`${PORT} is open!`);
+app.get('/axios', (req, res) => {
+  console.log(req.query);
+  res.send(req.query);
+});
+
+app.post('/axios', (req, res) => {
+  console.log(req.body);
+
+  // res.send(req.body);
+  res.send({ name: req.body.name, gender: req.body.gender, msg: '반가워!!' });
+});
+
+app.get('/fetch', (req, res) => {
+  console.log(req.query);
+  res.send(req.query);
+});
+
+app.post('/fetch', (req, res) => {
+  console.log(req.body);
+  res.send(req.body);
+});
+
+app.listen(PORT, function () {
+  console.log(`Port ${PORT} is opening!`);
 });
