9b1787a124c941142f078d7a08b133a5721119b8
jjsh03 <aberrant03@gmail.com>
2023-08-21 18:53:45 +0900

    study: 파일 업로드 실습

---
+++ 13-FILE-UPLOAD/practice.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const app = express();
+const PORT = 8080;
+
+// multer 관련 설정
+const multer = require('multer');
+const path = require('path');
+const upload = multer({
+  dest: 'uploads/',
+});
+
+// multer 세부 설정
+const uploadDetail = multer({
+  storage: multer.diskStorage({
+    destination(req, file, done) {
+      done(null, 'uploads/');
+    },
+    filename(req, file, done) {
+      const ext = path.extname(file.originalname);
+      done(null, path.basename(req.body.id, ext) + ext);
+    },
+  }),
+
+  limits: { fileSize: 5 * 1024 * 1024 }, // 5MB
+});
+
+app.set('view engine', 'ejs');
+app.use('/views', express.static(__dirname + '/views'));
+app.use('/uploads', express.static(__dirname + '/uploads'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.render('practice');
+});
+
+app.post('/profile', uploadDetail.single('userfile'), (req, res) => {
+  console.log(req.file);
+  console.log(req.body);
+
+  const fileName = req.file.filename;
+  const filePath = '/' + req.file.destination + fileName;
+  res.render('result', { user: req.body, filePath });
+});
+
+app.listen(PORT, function () {
+  console.log(`Port ${PORT} is opening!`);
+});
