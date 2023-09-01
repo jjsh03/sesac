17b26633294fc927f7441809aedc7e5922aeb9f5
jjsh03 <aberrant03@gmail.com>
2023-09-01 13:30:44 +0900

    study: views 폴더 등록 방법 수정

--- 13-FILE-UPLOAD/app.js
+++ 13-FILE-UPLOAD/app.js
@@ -30,7 +30,7 @@ const uploadDetail = multer({
 });
 
 app.set('view engine', 'ejs');
-app.use('/views', express.static(__dirname + '/views'));
+app.set('views', './views');
 app.use('/Uploads', express.static(__dirname + '/uploads'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
