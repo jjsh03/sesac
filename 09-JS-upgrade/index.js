52e4c2df4164ea8a6a583a7982a8c175afeaf9b4
jjsh03 <aberrant03@gmail.com>
2023-10-06 14:54:29 +0900

    study: 단축평가

--- 09-JS-upgrade/index.js
+++ 09-JS-upgrade/index.js
@@ -267,3 +267,38 @@ class Circle extends Shape {
 
 const cir = new Circle(2, 2, 5);
 console.log(cir.getArea());
+
+//////////////////////////////////
+// 단축 평가
+// &&, ||
+
+// A && B: 두 개의 피연산자 모두 t면 t 반환
+// A || B: 두 개의 피연산자 중에서 하나만 t여도 t 반환
+
+console.log(true && true); // true
+console.log(false && true); // false
+
+console.log(true || false); // true
+console.log(false || true); // true
+
+const xx = 5;
+const yy = 7;
+
+// 삼항연산자 예시
+const result1 = xx > yy ? 'xx가 큼!' : 'yy가 큼!';
+console.log(result1); // yy가 큼!
+
+// 단축평가 (&&, 논리곱)
+const result2 = xx > yy && 'xx가 큼!';
+console.log(result2); // false
+const result3 = xx < yy && 'yy가 큼!';
+console.log(result3); // yy가 큼
+
+// 단축형가 (||, 논리합)
+const result4 = xx || 100;
+console.log(result4); // 5
+
+const nameEx = '홍길동';
+const nameEx2 = null;
+console.log(nameEx || '이름없음'); // 홍길동
+console.log(nameEx2 || '이름없음'); // 이름없음
