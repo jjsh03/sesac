a61c8c1b6fdf5154aa295fe634425f0f25aea12a
jjsh03 <aberrant03@gmail.com>
2023-10-11 15:51:02 +0900

    study: react styledComponent

--- 29-styles/src/App.js
+++ 29-styles/src/App.js
@@ -1,5 +1,6 @@
 import CssModuleComponents from './CssModuleComponents';
 import SassComponents from './SassComponent';
+import StyledComponent from './StyledComponent';
 import './styles/App.css';
 
 function App() {
@@ -12,6 +13,9 @@ function App() {
 
       <h2>SASS Module</h2>
       <SassComponents />
+
+      <h2>Styled Component</h2>
+      <StyledComponent />
     </div>
   );
 }
